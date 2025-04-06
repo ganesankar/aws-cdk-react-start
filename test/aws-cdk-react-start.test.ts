9408ead22480dfdf39c594ea90ec5da41def2765
Ganesan K <ganesan.intech@gmail.com>
2025-04-06 14:53:25 -0700

    cdk init

---
+++ test/aws-cdk-react-start.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkReactStart from '../lib/aws-cdk-react-start-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-react-start-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkReactStart.AwsCdkReactStartStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

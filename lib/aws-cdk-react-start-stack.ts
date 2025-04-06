7f229f23bf8cab019d7b5355562f6db084e21fe8
Ganesan K <ganesan.intech@gmail.com>
2025-04-06 15:30:30 -0700

    Update aws-cdk-react-start-stack.ts

--- lib/aws-cdk-react-start-stack.ts
+++ lib/aws-cdk-react-start-stack.ts
@@ -1,5 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkReactStartStack extends cdk.Stack {
@@ -7,10 +11,14 @@ export class AwsCdkReactStartStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, "MyWebBucket", {
+      bucketName: "aws-cdk-react-start-web",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ACLS,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkReactStartQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  
   }
 }
